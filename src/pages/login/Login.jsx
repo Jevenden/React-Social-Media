60ba4bfb7084cc2c0748cd6577d7fae3653fa4cb
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-10-29 18:37:02 -0400

    Created and styled login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,26 @@ import "./login.scss"
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World</h1>
+                    <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing jsfbasojflnwosefkd!
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username"></input>
+                        <input type="password" placeholder="Password"></input>
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
