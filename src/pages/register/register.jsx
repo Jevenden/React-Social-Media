b781549a0bd0beb0ff53d28210b937483aa888cc
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-11-07 13:33:52 -0500

    squashing deployment issues

---
+++ src/pages/register/register.jsx
@@ -0,0 +1,33 @@
+import "./register.scss"
+import { Link } from "react-router-dom"
+
+const Register = () => {
+    return (
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <h1>Less Evil Social</h1>
+                    <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing jsfbasojflnwosefkd!
+                    </p>
+                    <span>Do you have an account?</span>
+                    <Link to="/login">
+                    <button>Login</button>
+                    </Link>
+                </div>
+                <div className="right">
+                    <h1>Register</h1>
+                    <form>
+                        <input type="text" placeholder="Username"></input>
+                        <input type="email" placeholder="Email"></input>
+                        <input type="password" placeholder="Password"></input>
+                        <input type="text" placeholder="Name"></input>
+                        <button>Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
